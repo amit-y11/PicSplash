669f1ed849931d534f92a3be1a9c02579c2d8e5a
unknown <yamit5869@protonmail.com>
2021-08-27 22:46:32 +0530

    fix theme mode issue

--- lib/utils/pref_manager.dart
+++ lib/utils/pref_manager.dart
@@ -38,4 +38,32 @@ class PrefManager {
       return [];
     }
   }
+
+  saveThemeIndex(int themeIndex) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt("themeIndex", themeIndex);
+  }
+
+  Future<int> getThemeIndex() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt("themeIndex");
+    if (themeIndex != null) {
+      return themeIndex;
+    } else
+      return 0;
+  }
+
+  saveAccentIndex(int accentIndex) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt("accentIndex", accentIndex);
+  }
+
+  Future<int> getAccentIndex() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final accentIndex = prefs.getInt("accentIndex");
+    if (accentIndex != null) {
+      return accentIndex;
+    } else
+      return 0;
+  }
 }
